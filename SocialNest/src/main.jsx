971cd66ef5d23788a31c673a2019a2032e3bd1c4
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-27 19:13:36 -0500

    setup project for SocialNest

---
+++ SocialNest/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Welcome to Social Nest</h1>
+  </StrictMode>
+);
